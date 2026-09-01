9a82c2a7246fc4b6b91498cdbfbbf0efbfa4c8be
Andrés Orozco <49706917+GnFelipe@users.noreply.github.com>
2026-09-01 16:29:31 -0500

    Update LiquidacionesControllers.cs

---
+++ src/Backend/Credilike.Api/Controllers/LiquidacionesControllers.cs
@@ -0,0 +1,6 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Credilike.Core.DTOs;
+using Credilike.Core.Interfaces;
